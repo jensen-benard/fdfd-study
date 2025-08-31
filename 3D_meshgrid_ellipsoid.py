bae5dc09834d85e4f95a8bd2992b98e794acf2dc
Jensen <jensen.benard@protonmail.com>
2025-08-31 12:23:05 +1200

    Cleanup

--- 3D_meshgrid_ellipsoid.py
+++ 3D_meshgrid_ellipsoid.py
@@ -5,38 +5,7 @@ import pyvista as pv
 
 ureg = UnitRegistry()
 
-
-
 # DASHBOARD
-
-# gridSizeX = 1
-# gridSizeY = 1
-
-# CRITICAL_DIMENSION_X = 1
-# CRITICAL_DIMENSION_Y = 1
-
-# CELL_RESOLUTION = 1 # Number of cells per smallest wavelength
-
-# REFRACTIVE_INDEX_MAX = 1
-
-# FREE_SPACE_WAVELENGTH = 1
-
-# # Set a cell size based on the smallest wavelength and cell resolution 
-# cellSizeX = FREE_SPACE_WAVELENGTH / REFRACTIVE_INDEX_MAX / CELL_RESOLUTION
-# cellSizeY = cellSizeX
-
-# # Readjust the cell size to snap to a critical dimension
-# cellCountXForCritDim = np.ceil(CRITICAL_DIMENSION_X / cellSizeX).astype(int)
-# cellCountYForCritDim = np.ceil(CRITICAL_DIMENSION_Y / cellSizeY).astype(int)
-# cellSizeX = CRITICAL_DIMENSION_X / cellCountXForCritDim
-# cellSizeY = CRITICAL_DIMENSION_Y / cellCountYForCritDim
-
-# # Readjusting grid size to snap to critical dimension (using the snapped cell size)
-# cellCountForGridX = np.ceil(gridSizeXX / cellSizeX).astype(int)
-# cellCountForGridY = np.ceil(gridSizeY / cellSizeY).astype(int)
-# gridSizeX = cellCountForGridX * cellSizeX
-# gridSizeY = cellCountForGridY * cellSizeY
-
 
 GRID_SIZE_X = 1
 GRID_SIZE_Y = 1
